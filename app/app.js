43caabd533f20fb5c8371fea4044e9128839b080
rahmatbrylyant975@gmail.com <rahmatbrylyant975@gmail.com>
2026-04-11 01:41:24 +0700

    scroll reveal done

--- app/app.js
+++ app/app.js
@@ -63,7 +63,7 @@ const scrollUp = () => {
 window.addEventListener("scroll", scrollUp);
 
 // SCROLL SECTIONS ACTIVE LINK
-const sections = document.getElementById("section[id]");
+const sections = document.querySelectorAll("section[id]");
 
 // Link the ID of each section (section id="home") to each link (a href="#home")
 // and activate the link with the class .active-link
@@ -75,7 +75,7 @@ const scrollActive = () => {
     const id = section.id, // id for each section
       top = section.offsetTop - 50, // Distance from the top edge
       height = section.offsetHeight, // Element height
-      link = document.querySelector(".nav__menu a[href*=" + id + "]"); //id nav link
+      link = document.querySelector('.nav__menu a[href*="' + id + '"]'); //id nav link
 
     if (!link) return;
 
@@ -87,3 +87,61 @@ const scrollActive = () => {
 };
 window.addEventListener("scroll", scrollActive);
 
+// SCROLL REVEAL ANIMATION
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2000,
+  delay: 300,
+});
+
+sr.reveal(".popular__swiper, .footer__container,. footer__copy");
+sr.reveal("home__shape", { origin: "bottom" });
+sr.reveal("home__menu", { delay: 1000, distance: "2000px", duration: 1500 });
+sr.reveal(".home__splash", { delay: 1600, scale: 0, duration: 1500 });
+sr.reveal(".home__broccoli-1, .home__broccoli-2", {
+  delay: 2200,
+  scale: 0,
+  duration: 1500,
+  rotate: { z: 180 },
+});
+sr.reveal(".home__mushrooms-1, .home__mushrooms-2", {
+  delay: 2000,
+  scale: 0,
+  duration: 1500,
+  rotate: { z: 180 },
+});
+sr.reveal(".home__lettuce", {
+  delay: 2000,
+  scale: 0,
+  duration: 1500,
+  rotate: { z: 180 },
+});
+sr.reveal(".home__title", {
+  delay: 3000,
+});
+sr.reveal(".home__data,.home__sticker", {
+  delay: 4000,
+});
+sr.reveal(".about__data", {
+  origin: "left",
+});
+sr.reveal(".about__images", {
+  origin: "right",
+});
+sr.reveal(".about__menu", {
+  delay: 1000,
+});
+sr.reveal(".about__broccoli-1,.about__broccoli-2", {
+  delay: 1400,
+});
+sr.reveal(".products__card,.contact__info", {
+  interval: 100,
+});
+sr.reveal(".contact__shape", {
+  delay: 600,
+  scale: 0,
+});
+sr.reveal(".contact__delivery", {
+  delay: 1200,
+});
